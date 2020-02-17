4b87ff6e100a2ab94226a0f8060a90d7c81ee3a1
Shnurk <47347453+Shnurk@users.noreply.github.com>
2020-02-17 14:34:14 +0300

    IT 0.4
    add updated menu + picts

--- plinor/bizz/view/view_menu.php
+++ plinor/bizz/view/view_menu.php
@@ -16,40 +16,40 @@
                 <a href="/plinor/main/them/games" class="nav_one">расходные материалы</a>
         </div>
         <div class="nav_setting nav_edge">
-            <?if(isset($_COOKIE["hash"])&&($_COOKIE["hash"]!="")):?>
+            <?php if(isset($_COOKIE["hash"])&&($_COOKIE["hash"]!="")):?>
                 <a href="/plinor/cabinet/view/id" class="nav_one">кабинет</a>
                 <a href="#">|</a>
                 <a href="/plinor/mail" class="nav_one">почта</a>
                 <a href="#">|</a>
                 <a href="/plinor/history" class="nav_one">история</a>
                 <a href="#">|</a>
-            <?endif;?>
-                <?if(isset($_COOKIE["hash"])&&($_COOKIE["hash"]!="")):?>
+            <?php endif;?>
+                <?php if(isset($_COOKIE["hash"])&&($_COOKIE["hash"]!="")):?>
                     <a href="/plinor/enter/exit" class="nav_one">выйти</a>
-                <?else:?>
+                <?php else:?>
                     <a href="/plinor/enter" class="nav_one">войти</a>
-                <?endif;?>
+                <?php endif;?>
         </div>
     </div>
 
     <div class="hot_news">
 
         <div class="hot_value hot_grey nav_edge">
-            <?if(isset($_SESSION['RULES'])&&($_SESSION['RULES']>=4)):?>
+            <?php if(isset($_SESSION['RULES'])&&($_SESSION['RULES']>=4)):?>
             <a  href="/plinor/list/order" class="hot_active">Заявки</a>
-            <?endif;?>
+            <?php endif;?>
         </div>
         <div class="hot_jump">
             <img src="/plinor/pic/web_logo/tea.gif">
             <p>Macbook Pro 9 зарезервирован с 26.07.20 по 28.07.20 Ивановой И.И.</p>
         </div>
         <div class="nav_setting hot_grey nav_edge">
-        <?if(isset($_SESSION['RULES'])&&($_SESSION['RULES']>=4)):?>
+        <?php if(isset($_SESSION['RULES'])&&($_SESSION['RULES']>=4)):?>
                 <a href="#">Список:</a>
                 <a href="/plinor/list/tech" class="hot_active">Техника</a>
                 <a href="#">|</a>
                 <a href="/plinor/list/user" class="hot_active">Пользователи</a>
-            <?endif;?>
+            <?php endif;?>
         </div>
     </div>
 </header>
