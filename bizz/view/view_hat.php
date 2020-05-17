a44cd45bedf46f2a01bfe1e46244fa6326fc70d7
Shnurk <47347453+Shnurk@users.noreply.github.com>
2020-05-18 00:46:37 +0300

    Update view_hat.php

--- bizz/view/view_hat.php
+++ bizz/view/view_hat.php
@@ -2,19 +2,19 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/landing.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/enter.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/menu.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/article.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/art_man.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/list.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/cabinet.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE;?>/css/mail.css">
-    <link rel="shortcut icon"  href="<?php echo URL_BASE;?>/pic/web_logo/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="/css/landing.css">
+    <link rel="stylesheet" type="text/css" href="/css/enter.css">
+    <link rel="stylesheet" type="text/css" href="/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="/css/article.css">
+    <link rel="stylesheet" type="text/css" href="/css/art_man.css">
+    <link rel="stylesheet" type="text/css" href="/css/list.css">
+    <link rel="stylesheet" type="text/css" href="/css/cabinet.css">
+    <link rel="stylesheet" type="text/css" href="/css/mail.css">
+    <link rel="shortcut icon"  href="/pic/web_logo/logo.ico" type="image/x-icon">
     <script src="https://kit.fontawesome.com/3c32a2ea49.js" crossorigin="anonymous"></script>
     <title>Плинор-техника</title>
 </head>
 <body>
-<script src="<?php echo URL_BASE;?>/js/jq.js"></script>
-<script src="<?php echo URL_BASE;?>/js/tag.js"></script>
-<script src="<?php echo URL_BASE;?>/js/file_name.js"></script>
+<script src="/js/jq.js"></script>
+<script src="/js/tag.js"></script>
+<script src="/js/file_name.js"></script>
